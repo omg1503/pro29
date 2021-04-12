51c6414198b43ee878e3b25b237a10fd2686fcfc
omg1503 <76679434+omg1503@users.noreply.github.com>
2021-04-12 22:07:26 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,34 @@
+class Slingshot{
+    constructor(bodyA, pointB){
+     
+        var options = {
+        
+        length : 1,
+        stiffness : 0.04,
+        bodyA: bodyA,
+        pointB: pointB
+        }
+
+        this.sling = Constraint.create(options);
+        this.pointB = pointB;
+            World.add(world, this.sling);
+    }
+
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    fly(){
+          this.sling.bodyA = null;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            
+            strokeWeight(4);
+            stroke("turquoise");
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+}
